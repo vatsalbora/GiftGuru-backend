2b4017810f47a3ef06b01a886dd1101b44a619cd
Suthan <suthan.sivekumaar@gmail.com>
2023-03-09 01:31:02 -0500

    Displaying rows from heroku db

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 import pg from "pg";
-const { Client } = pg;
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+const { Pool } = pg;
+const pool = new Pool({
+  connectionString: "postgres://yvkmdeopnjwoqo:c051cfcccd1c290f251785d6aef2d9069abbf8b8edeb34add361043fbbaae827@ec2-52-54-212-232.compute-1.amazonaws.com:5432/d3bhdcrs7e1dga",
   ssl: { rejectUnauthorized: false },
 });
 
-export default client;
+export default pool;
