612acff1f0841e0fe4e301540701701443b1bf22
ewnagteg <ewnagteg@users.noreply.github.com>
2023-03-09 16:45:08 -0500

    fix env file

--- server/db.js
+++ server/db.js
@@ -1,7 +1,11 @@
 import pg from "pg";
+import dotenv from "dotenv";
 const { Pool } = pg;
+dotenv.config();
+const password = process.env.PASSWORD;
+console.log(password);
 const pool = new Pool({
-  connectionString: "postgres://yvkmdeopnjwoqo:c051cfcccd1c290f251785d6aef2d9069abbf8b8edeb34add361043fbbaae827@ec2-52-54-212-232.compute-1.amazonaws.com:5432/d3bhdcrs7e1dga",
+  connectionString: `postgres://yvkmdeopnjwoqo:${password}@ec2-52-54-212-232.compute-1.amazonaws.com:5432/d3bhdcrs7e1dga`,
   ssl: { rejectUnauthorized: false },
 });
 
